40630fbbb3b1d5546b2e3a4bd39fdbfd3d18a18f
rip-embraer <planejamentoportal@gmail.com>
2024-07-29 16:21:53 -0300

    first test

---
+++ New Portal/components/arrow-down-animation/arrow-down-animation.js
@@ -0,0 +1,24 @@
+console.log("animação arrow carrregada");
+
+const linkArrowAnimation = document.createElement('link');
+linkArrowAnimation.rel = 'stylesheet';
+linkArrowAnimation.href = './components/arrow-down-animation/arrow-down-animation.css';
+document.head.appendChild(linkArrowAnimation);
+
+
+const elementsArrowAnimation = document.getElementsByClassName('animation-down-container');
+for (let i = 0; i < elementsArrowAnimation.length; i++) {
+    elementsArrowAnimation[i].innerHTML = `
+        <div class="animation-down-back">
+            <div class="animation-down-arrow">
+                V
+            </div>
+            <div class="animation-down-arrow">
+                V
+            </div>
+            <div class="animation-down-arrow">
+                V
+            </div>
+        </div>
+    `;
+}
